d9aac744baa0450823ea4124c14a940880c03e59
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-03-05 20:26:49 +0100

    scorpio: Add fingerprint
    Change-Id: If3b2527585ec32caddc457e1293f86fd27b6f1af

--- lineage.mk
+++ lineage.mk
@@ -4,8 +4,15 @@ $(call inherit-product, device/xiaomi/scorpio/full_scorpio.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_scorpio
-PRODUCT_BRAND := xiaomi
-PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_DEVICE := scorpio
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Mi Note 2
+PRODUCT_MANUFACTURER := xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="scorpio" \
+    PRODUCT_NAME="scorpio" \
+    BUILD_FINGERPRINT="scorpio-user 6.0.1 MXB48T 7.5.4 release-keys" \
+    PRIVATE_BUILD_DESC="Xiaomi/scorpio/scorpio:6.0.1/MXB48T/7.5.4:user/release-keys"
