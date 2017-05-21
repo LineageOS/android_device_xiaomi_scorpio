4ee7858d220a635f8ee0ecef79eb28a42ff66593
Bruno Martins <bgcngm@gmail.com>
2017-05-21 15:54:51 +0100

    scorpio: Update product properties according to stock
    Change-Id: I0aa766870d517a54d9b6b316da689066a074dbbd

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Xiaomi MI 5s Plus"
+#define BTM_DEF_LOCAL_NAME   "Xiaomi MI Note 2"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BLUETOOTH_QCOM_SW TRUE
 #define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
--- lineage.mk
+++ lineage.mk
@@ -5,14 +5,14 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_scorpio
 PRODUCT_DEVICE := scorpio
-PRODUCT_BRAND := xiaomi
+PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI Note 2
-PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="scorpio" \
     PRODUCT_NAME="scorpio" \
-    BUILD_FINGERPRINT="scorpio-user 6.0.1 MXB48T V8.2.6.0.MADCNDL release-keys" \
-    PRIVATE_BUILD_DESC="Xiaomi/scorpio/scorpio:6.0.1/MXB48T/V8.2.6.0.MADCNDL:user/release-keys"
+    BUILD_FINGERPRINT="Xiaomi/scorpio/scorpio:6.0.1/MXB48T/V8.2.6.0.MADCNDL:user/release-keys" \
+    PRIVATE_BUILD_DESC="scorpio-user 6.0.1 MXB48T V8.2.6.0.MADCNDL release-keys"
