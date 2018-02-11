f552bd255816934301df6b932ce4ad83c280ddb0
Bruno Martins <bgcngm@gmail.com>
2018-02-11 23:43:26 +0000

    scorpio: Split device specific and system props
    Change-Id: I023f21932a3524c15f1b0af11fd71107152cab5f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,7 +34,7 @@ TARGET_KERNEL_CONFIG := scorpio_defconfig
 NXP_CHIP_TYPE := 2
 
 # Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # inherit from the proprietary version
 -include vendor/xiaomi/scorpio/BoardConfigVendor.mk
