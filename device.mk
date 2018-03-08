feb95f50e311b1132b70bf824c2c044e190b75f2
Giuseppe Barillari <joe2k01dev@gmail.com>
2018-03-08 19:29:47 +0100

    scorpio: rootdir: Cleanup
     * Neither of the nodes is actually present
    
    Change-Id: Ibddde66feebf84bd6b04ff84382ef6706fe29af0
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- device.mk
+++ device.mk
@@ -39,10 +39,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ConfigPanel
 
-# Device init scripts
-PRODUCT_PACKAGES += \
-    init.target.rc
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service
