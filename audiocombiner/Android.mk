c7aa4e864397234b66c0922dd9cb1fe32d645f9b
Giuseppe Barillari <joe2k01dev@gmail.com>
2018-05-25 15:38:13 +0200

    scorpio: Add AudioCombiner to handle audio channels combination
    Change-Id: Ibf244753981d0ec794ab0a69287ebe72c4207956
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

---
+++ audiocombiner/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PACKAGE_NAME := AudioCombiner
+
+LOCAL_USE_AAPT2 := true
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    org.lineageos.platform.internal
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_PACKAGE)
