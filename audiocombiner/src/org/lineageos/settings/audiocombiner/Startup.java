135519db45717e43be868444aee0490f2980f305
Giuseppe Barillari <joe2k01dev@gmail.com>
2018-05-25 15:38:13 +0200

    scorpio: Add AudioCombiner to handle audio channels combination
    Change-Id: Ibf244753981d0ec794ab0a69287ebe72c4207956
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

---
+++ audiocombiner/src/org/lineageos/settings/audiocombiner/Startup.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.audiocombiner;
+
+import android.bluetooth.BluetoothA2dp;
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+public class Startup extends BroadcastReceiver {
+
+    private AudioCombiner mAudioCombiner;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        mAudioCombiner = new AudioCombiner();
+        IntentFilter audioFilter = new IntentFilter();
+        audioFilter.addAction(AudioManager.ACTION_HEADSET_PLUG);
+        audioFilter.addAction(BluetoothA2dp.ACTION_CONNECTION_STATE_CHANGED);
+        audioFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        context.getApplicationContext().registerReceiver(mAudioCombiner, audioFilter);
+
+        Settings.System.putIntForUser(context.getContentResolver(),
+                 Settings.System.MASTER_MONO, 1, UserHandle.USER_CURRENT);
+    }
+}
