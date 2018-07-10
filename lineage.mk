de7c5a9132412cbe9f6bc6447429b44fade255b9
Bruno Martins <bgcngm@gmail.com>
2018-07-10 19:02:16 +0100

    scorpio: Update build fingerprint from Oreo-based MIUI 9 Global Stable
     * Newly released, just few days after the last build fingerprint update
    
    Change-Id: I6b23c550196e9490d69e99a23eed4471514b6da2

--- lineage.mk
+++ lineage.mk
@@ -14,7 +14,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="scorpio" \
     PRODUCT_NAME="scorpio" \
-    PRIVATE_BUILD_DESC="scorpio-user 7.0 NRD90M V9.5.4.0.NADMIFA release-keys"
+    PRIVATE_BUILD_DESC="scorpio-user 8.0.0 OPR1.170623.032 V9.6.1.0.OADMIFD release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/scorpio/scorpio:7.0/NRD90M/V9.5.4.0.NADMIFA:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/scorpio/scorpio:8.0.0/OPR1.170623.032/V9.6.1.0.OADMIFD:user/release-keys"
