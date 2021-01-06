f50436fc8ad947d89953ea7c09ca01c3e4f01181
Bruno Martins <bgcngm@gmail.com>
2021-01-06 15:01:49 +0000

    scorpio: Stop building ConfigPanel
    The hardware buttons swap feature is now implemented in the touch HAL,
    so this package is no longer necessary.
    
    Change-Id: Ie97dbdde5e7dbaf94232de973380f17091b2e155

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -37,10 +37,6 @@ PRODUCT_COPY_FILES += \
 # Consumer IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-service.xiaomi
-
-# ConfigPanel
-PRODUCT_PACKAGES += \
-    ConfigPanel
 
 # Fingerprint
 PRODUCT_PACKAGES += \
