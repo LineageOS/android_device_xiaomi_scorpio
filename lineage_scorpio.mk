ea1fd261223c8e8f03e9dee781af31f70fb749d0
LuK1337 <priv.luk@gmail.com>
2025-03-08 02:28:36 +0100

    scorpio: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38
    Signed-off-by: Onelots <onelots@onelots.fr>

--- lineage_scorpio.mk
+++ lineage_scorpio.mk
@@ -30,8 +30,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="scorpio" \
-    PRIVATE_BUILD_DESC="scorpio-user 8.0.0 OPR1.170623.032 V9.6.1.0.OADMIFD release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Xiaomi/scorpio/scorpio:8.0.0/OPR1.170623.032/V9.6.1.0.OADMIFD:user/release-keys"
+    BuildDesc="scorpio-user 8.0.0 OPR1.170623.032 V9.6.1.0.OADMIFD release-keys" \
+    BuildFingerprint=Xiaomi/scorpio/scorpio:8.0.0/OPR1.170623.032/V9.6.1.0.OADMIFD:user/release-keys
